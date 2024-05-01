8a2bb32a0510940fa7cf7668fc252320c3a61f67
naviadev <qo2oq1020@gmail.com>
2024-05-01 20:32:20 +0900

    make right button bar

---
+++ MacOSTopBar/MacOSTopBar/MainCheckList.swift
@@ -0,0 +1,8 @@
+//
+//  MainCheckList.swift
+//  MacOSTopBar
+//
+//  Created by 배성빈 on 5/1/24.
+//
+
+import Foundation
---
+++ MacOSTopBar/MacOSTopBar/SettingBar.swift
@@ -0,0 +1,8 @@
+//
+//  SettingBar.swift
+//  MacOSTopBar
+//
+//  Created by 배성빈 on 5/1/24.
+//
+
+import Foundation
