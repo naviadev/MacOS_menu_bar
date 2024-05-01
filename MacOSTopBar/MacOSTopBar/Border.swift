65b56dc376785e8c3d68460aeb79f6e311c76e58
naviadev <qo2oq1020@gmail.com>
2024-05-01 19:30:26 +0900

    팝업 커스텀 스타일 적용

---
+++ MacOSTopBar/MacOSTopBar/Border.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MacOSTopBar
+//
+//  Created by 배성빈 on 5/1/24.
+//
+
+import Foundation
---
+++ MacOSTopBar/MacOSTopBar/ExpBar.swift
@@ -0,0 +1,8 @@
+//
+//  ExpBar.swift
+//  MacOSTopBar
+//
+//  Created by 배성빈 on 5/1/24.
+//
+
+import Foundation
