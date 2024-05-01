ecf346b78dd8c7c2ef2965ed9f73b70b099c6178
naviadev <qo2oq1020@gmail.com>
2024-05-01 19:12:17 +0900

    일부 코드 정리

--- MacOSTopBar/MacOSTopBar/MenuController.swift
+++ MacOSTopBar/MacOSTopBar/MenuController.swift
@@ -8,25 +8,31 @@
 import SwiftUI
 
 @main
-struct MenuMap: App {
-    var body: some Scene {
+struct MenuController: App {
+    
+    var body : some Scene{
+        
         MenuBarExtra {
-            ContentView()
-        } label: {
+            AppMenu()
+        } 
+        label : {
             let image: NSImage = {
                 let ratio = $0.size.height / $0.size.width
                 $0.size.height = 18
                 $0.size.width = 18 / ratio
                 return $0
             }(NSImage(named: "AppIcon")!)
-            
             Image(nsImage: image)
         }
+        
     }
     
-    struct AppMenu: View {
-        var body: some View {
-            ContentView()
-        }
+    
+}
+
+struct AppMenu: View {
+    var body: some View {
+        Text("App Menu Item")
     }
 }
+
---
+++ MacOSTopBar/MacOSTopBar/MenuScene.swift
@@ -0,0 +1,17 @@
+//
+//  MenuScene.swift
+//  MacOSTopBar
+//
+//  Created by 배성빈 on 5/1/24.
+//
+
+import SwiftUI
+
+struct MenuScene : View{
+    var body: some View {
+            Text("123")
+    }
+}
+
+
+
